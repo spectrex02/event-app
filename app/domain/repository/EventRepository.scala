732960fd38753480c4906a3641a1a076023ac1c8
spectrex02 <iscale821@gmail.com>
2019-08-07 18:38:51 +0900

    2019-08-07.1

--- app/domain/repository/EventRepository.scala
+++ app/domain/repository/EventRepository.scala
@@ -1,9 +1,12 @@
 package domain.repository
 
+import java.io.ByteArrayInputStream
+import java.sql.Timestamp
 import java.{io, sql}
 
 import domain.entity._
 import java.time.LocalDateTime
+import java.util.Date
 
 import scalikejdbc._
 
@@ -16,7 +19,7 @@ object EventRepository {
     val tmp: Option[Event] = sql"select * from event where id = $id".map{
       rs => Event(id = rs.get("id"),
                   eventName = rs.get("event_name"),
-        candidateDates = rs.array("candidate_dates").asInstanceOf[Array[LocalDateTime]].toSeq.map(date => date -> Seq.empty[Vote]).toMap,
+          candidateDates = rs.string("candidate_dates").split(",").toSeq.map(date => DateFormatter.string2date(date) -> Seq.empty[Vote]).toMap,
         deadline = rs.get("deadline"),
         comment = rs.get("comment"))
     }.single().apply()
@@ -30,8 +33,10 @@ object EventRepository {
       date
     }.mapValues(_.map(_._2))
 
+
+
     tmp match {
-      case Some(e) => Some(e.copy(candidateDates = v2))
+      case Some(e) => if(v2 == Map.empty[LocalDateTime, Seq[Vote]]) Some(e) else Some(e.copy(candidateDates = v2))
       case None => None
     }
   }
@@ -42,38 +47,58 @@ object EventRepository {
   }
 
   //新規イベントをDBに追加
-  def insertEvent(event: Event, plannerName: String)(implicit session: AutoSession): SQLExecution = {
-    val dates = event.candidateDates.keys.map(date => DateFormatter.date2string(date))
-    val dl = DateFormatter.date2string(event.deadline)
-    sql"insert into event (event_name, candidate_dates, deadline, comment, planner) values ($event.eventName, $dates, $dl, $event.comment, $plannerName)".execute()
+  def insertEvent(event: Event, plannerName: String)(implicit session: AutoSession): Boolean = {
+//    val dates: Array[Timestamp] = event.candidateDates.keys.map(date => Timestamp.valueOf(date)).toArray
+    val dates: String = event.candidateDates.keys.map(date => DateFormatter.date2string(date)).mkString(",")
+    val dl: String = DateFormatter.date2string(event.deadline)
+    sql"insert into event (event_name, candidate_dates, deadline, comment, planner) values (${event.eventName}, ${dates}, ${dl}, ${event.comment}, ${plannerName})".execute().apply()
   }
 
+//  def insertEvent2(e: Event, plannerName: String): Boolean = {
+//    val dates: Array[String] = e.candidateDates.keys.map(date => DateFormatter.date2string(date)).toArray
+//
+//
+////    withSQL {
+////      insert.into(Event).columns(
+////        column.event_name,
+////        column.candidate_dates,
+////        column.deadline,
+////        column.comment,
+////        column.planner
+////      ).values(
+////        e.eventName,
+////        dates,
+////        DateFormatter.date2string(e.deadline),
+////        e.comment,
+////        plannerName
+////      )
+////    }.execute().apply()
+//  }
   //イベント情報の更新をデータベースに反映させる
-  def updateEvent(event: Event)(implicit session: AutoSession) = {
-    sql"update table event set where event_name = $event.eventName, id = $event.id"
-  }
+  def updateEvent(event: Event)(implicit session: AutoSession): Boolean = sql"update table event set where event_name = $event.eventName, id = $event.id".execute().apply()
+
   //イベントの削除をDBに反映
-  def deleteEvent(eventId: Int)(implicit session: AutoSession): SQLExecution = sql"delete from event where id = $eventId".execute()
+  def deleteEvent(eventId: Int)(implicit session: AutoSession): Boolean = sql"delete from event where id = $eventId".execute().apply()
   //イベントの投票期間締め切り
-  def closeEvent(eventId: Int)(implicit session: AutoSession): SQLExecution = sql"update table event set status = false where id = $eventId".execute()
+  def closeEvent(eventId: Int)(implicit session: AutoSession): Boolean = sql"update table event set status = false where id = $eventId".execute().apply()
 
   //投票に関するクエリを実行するメソッドたち
   //投票をDBに反映させる
-  def insertVoting(eventId: Int, votingDate: LocalDateTime, vote: Vote) = {
+  def insertVoting(eventId: Int, votingDate: LocalDateTime, vote: Vote)(implicit session: AutoSession): Boolean = {
     val date = DateFormatter.date2string(votingDate)
-    sql"insert into vote (id, participant_name, voting_date, voting_status) values ($eventId, $vote.name, $date, $vote.voting_status)".execute()
+    sql"insert into vote (id, participant_name, voting_date, voting_status) values ($eventId, $vote.name, $date, $vote.voting_status)".execute().apply()
   }
 
   //投票内容の変更をDBに反映させる
-  def updateVoting(eventId: Int, votingDate: LocalDateTime, vote: Vote) = {
+  def updateVoting(eventId: Int, votingDate: LocalDateTime, vote: Vote)(implicit session: AutoSession): Boolean = {
     val date = DateFormatter.date2string(votingDate)
     val status = Util.VotingValue2Int(vote.votingStatus)
-    sql"update table event set voting_status = $status where id = $eventId and participant_name = $vote.name and voting_date = $date".execute()
+    sql"update table event set voting_status = $status where id = $eventId and participant_name = $vote.name and voting_date = $date".execute().apply()
   }
 
   //投票内容の削除をDBに反映
-  def deleteVoting(eventId: Int, votingDate: LocalDateTime, vote: Vote) = {
+  def deleteVoting(eventId: Int, votingDate: LocalDateTime, vote: Vote)(implicit session: AutoSession): Boolean = {
     val date = DateFormatter.date2string(votingDate)
-    sql"delete from event where id = $eventId and participant_name = $vote.name and voting_date = $date".execute()
+    sql"delete from event where id = $eventId and participant_name = $vote.name and voting_date = $date".execute().apply()
   }
 }
--- test/domain/repository/EventRepositorySpec.scala
+++ test/domain/repository/EventRepositorySpec.scala
@@ -2,12 +2,27 @@ package domain.repository
 
 import java.time.LocalDateTime
 
+import domain.entity._
 import org.scalatest.{FlatSpec, Matchers}
-import scalikejdbc.AutoSession
+import scalikejdbc._
 
 class EventRepositorySpec extends FlatSpec with Matchers {
-  "find" should "return option(event)" in {
-    implicit val session = AutoSession
-    assert(EventRepository.find(100) == Seq.empty[LocalDateTime])
+
+  //connection
+  Class.forName("org.postgresql.Driver")
+  ConnectionPool.singleton("jdbc:postgresql://localhost:5432/eventapp", "tomoya", "tomoya")
+  implicit val session = AutoSession
+//  "find" should "return option(event)" in {
+//    assert(EventRepository.find(100).get == Event(100, "test event", Map(DateFormatter.string2date("2019-08-01 00:00") -> Seq.empty[Vote], DateFormatter.string2date("2019-08-10 00:00") -> Seq.empty[Vote]), DateFormatter.string2date("2019-07-31 00:00"), "test comment"))
+//  }
+//
+//  "findAll" should "return Seq[Event]" in {
+//    assert(EventRepository.findAll() == Seq(Event(100, "test event", Map(DateFormatter.string2date("2019-08-01 00:00") -> Seq.empty[Vote], DateFormatter.string2date("2019-08-10 00:00") -> Seq.empty[Vote]), DateFormatter.string2date("2019-07-31 00:00"), "test comment")))
+//  }
+
+  "insertEvent" should "return Boolean" in {
+    val newEvent = Event(-1, "insertEvent", Map(DateFormatter.string2date("2019-08-12 10:00") -> Seq.empty[Vote]), DateFormatter.string2date("2019-08-07 17:00"), "insert event test")
+
+    assert(EventRepository.insertEvent(newEvent, "tester for insert") == true)
   }
 }
